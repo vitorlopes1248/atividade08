01513c31128e2ca051a6209e5f1bf5807852b817
vitorlopes1248 <vitorlopes1248@gmail.com>
2024-09-04 16:01:35 -0300

    atividade08 real pra dolar

---
+++ atividade.py
@@ -0,0 +1,7 @@
+# Imagine que você está em uma viagem para os Estados Unidos e precisa converter o valor em reais para dólares. Crie um programa que receba o valor em reais e a taxa de câmbio atual, e exiba o valor equivalente em dólares.
+
+real = int(input("digite quantos reais que vai converter: "))
+
+valor = real /5.63
+
+print("Seu valor em dolar: ", valor)
